d3e659b0178676d7dd0e3295add97afdca87b310
thinkingPhysics <96352403+thinkingPhysics@users.noreply.github.com>
2022-02-28 15:14:48 +0400

    ÐДобаДобÐДобавлено создание пользователя в create_database.sql. ПосÐИсправлен config.ini в соответствии с изменениями.

--- tools/sql/create_database.sql
+++ tools/sql/create_database.sql
@@ -1,1 +1,6 @@
 create database loandepositrequest;
+CREATE USER 'ldr'@'localhost' IDENTIFIED BY 'Asx919zx';
+CREATE USER 'ldr'@'%' IDENTIFIED BY 'Asx919zx';
+GRANT ALL PRIVILEGES ON loandepositrequest . * TO 'ldr'@'localhost';
+GRANT ALL PRIVILEGES ON loandepositrequest . * TO 'ldr'@'%';
+FLUSH PRIVILEGES;
